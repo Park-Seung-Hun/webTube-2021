ad26abd9464b939a5a51012a223c9ef0f807c291
ParkSH <psh19930326@gmail.com>
2021-03-04 17:18:42 +0900

    #2.5 ES6 on NodeJS using Babel

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
-const express = require("express"); // node module을 'express' 폴더에서 가져오는 것.
+import express from "express"; // node module을 'express' 폴더에서 가져오는 것.
 const app = express(); // express를 실행해서 app를 만든 것.
 
 const PORT = 4000;
 
-function handleListening() {
+const handleListening = () =>
   console.log(`Listening on: http://localhost:${PORT}`);
-}
-
-function handleHome(req, res) {
-  res.send("Hello my home");
-}
-
-function handleProfile(req, res) {
-  res.send("You are on my profile");
-}
+const handleHome = (req, res) => res.send("I'm hungry");
+const handleProfile = (req, res) => res.send("You are on my profile"); // es6 화살표 함수
 
 app.get("/", handleHome); // '/'(root)에 접근할 떄 동작.
 app.get("/profile", handleProfile); // '/profile'에 접근할 떄 동작.
