09022ef360629f0751569f5aa1300c05f30440d0
ParkSH <psh19930326@gmail.com>
2021-03-04 15:42:27 +0900

    feat: #2.3 Your First Express Serve

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("hi");
+const express = require("express"); // node module을 'express' 폴더에서 가져오는 것.
+const app = express(); // express를 실행해서 app를 만든 것.
+
+const PORT = 4000;
+function handleListening() {
+  console.log(`Listening on: http://localhost:${PORT}`);
+}
+app.listen(PORT, handleListening);
