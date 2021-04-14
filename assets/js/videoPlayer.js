11680b6c8c880f5841a82b47508a0c96778c8f8a
Park-Seung-Hun <psh19930326@gmail.com>
2021-04-14 21:44:50 +0900

    #9.3 Enter Fullscreen Exit Fullscreen

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -2,7 +2,9 @@ const videoContainer = document.getElementById("jsVideoPlayer");
 const videoPlayer = document.querySelector("#jsVideoPlayer video");
 const playBtn = document.getElementById("jsPlayButton");
 const volumeBtn = document.getElementById("jsVolumeBtn");
+const fullScrnBtn = document.getElementById("jsFullScreen");
 
+/* playBtn 동작 */
 function handlePlayClick() {
   if (videoPlayer.paused) {
     /* 비디오가 멈춰있는 경우 */
@@ -15,6 +17,7 @@ function handlePlayClick() {
   }
 }
 
+/* volumeBtn 동작 */
 function handleVolumeClick() {
   if (videoPlayer.muted) {
     /* 비디오가 음소거 되어있는 경우 */
@@ -27,12 +30,31 @@ function handleVolumeClick() {
   }
 }
 
+/* fullScreen 동작 */
+
+// 1. FullScreen 사용 x
+function exitFullScreen() {
+  fullScrnBtn.innerHTML = '<i class="fas fa-expand"></i>';
+  fullScrnBtn.addEventListener("click", goFullScreen);
+  document.webkitExitFullscreen();
+}
+
+// 2. FullScreen 사용
+function goFullScreen() {
+  videoContainer.webkitRequestFullscreen();
+  fullScrnBtn.innerHTML = '<i class="fas fa-compress"></i>';
+  fullScrnBtn.removeEventListener("click", goFullScreen);
+  fullScrnBtn.addEventListener("click", exitFullScreen);
+}
+
+/* init() 동작 */
 function init() {
   playBtn.addEventListener("click", handlePlayClick);
   volumeBtn.addEventListener("click", handleVolumeClick);
+  fullScrnBtn.addEventListener("click", goFullScreen);
 }
 
-/* 1. 비디오가 존재할 경우 init() 실행 */
+/* 비디오가 존재할 경우 init() 실행 */
 if (videoContainer) {
   init();
 }
