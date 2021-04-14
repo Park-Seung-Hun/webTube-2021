4296c5b04ef459e7a9f19eee2965c6cfa7d315aa
Park-Seung-Hun <psh19930326@gmail.com>
2021-04-14 21:33:23 +0900

    #9.2 Mute Unmute

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -1,6 +1,7 @@
 const videoContainer = document.getElementById("jsVideoPlayer");
 const videoPlayer = document.querySelector("#jsVideoPlayer video");
 const playBtn = document.getElementById("jsPlayButton");
+const volumeBtn = document.getElementById("jsVolumeBtn");
 
 function handlePlayClick() {
   if (videoPlayer.paused) {
@@ -14,8 +15,21 @@ function handlePlayClick() {
   }
 }
 
+function handleVolumeClick() {
+  if (videoPlayer.muted) {
+    /* 비디오가 음소거 되어있는 경우 */
+    videoPlayer.muted = false;
+    volumeBtn.innerHTML = `<i class="fas fa-volume-up"></i>`;
+  } else {
+    /* 비디오의 음량이 출력되는 경우 */
+    videoPlayer.muted = true;
+    volumeBtn.innerHTML = `<i class="fas fa-volume-mute"></i>`;
+  }
+}
+
 function init() {
   playBtn.addEventListener("click", handlePlayClick);
+  volumeBtn.addEventListener("click", handleVolumeClick);
 }
 
 /* 1. 비디오가 존재할 경우 init() 실행 */
