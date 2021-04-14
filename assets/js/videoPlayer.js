e41ba50bc0e48d7b7cfc54859b971404e91bf5d8
Park-Seung-Hun <psh19930326@gmail.com>
2021-04-14 22:14:05 +0900

    #9.4 Total Time and Current Time

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -3,6 +3,8 @@ const videoPlayer = document.querySelector("#jsVideoPlayer video");
 const playBtn = document.getElementById("jsPlayButton");
 const volumeBtn = document.getElementById("jsVolumeBtn");
 const fullScrnBtn = document.getElementById("jsFullScreen");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
 
 /* playBtn 동작 */
 function handlePlayClick() {
@@ -36,22 +38,86 @@ function handleVolumeClick() {
 function exitFullScreen() {
   fullScrnBtn.innerHTML = '<i class="fas fa-expand"></i>';
   fullScrnBtn.addEventListener("click", goFullScreen);
-  document.webkitExitFullscreen();
+
+  /* 브라우저 호환 */
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) {
+    document.msExitFullscreen();
+  }
 }
 
 // 2. FullScreen 사용
 function goFullScreen() {
-  videoContainer.webkitRequestFullscreen();
+  /* 브라우저 호환 */
+  if (videoContainer.requestFullscreen) {
+    videoContainer.requestFullscreen();
+  } else if (videoContainer.mozRequestFullScreen) {
+    videoContainer.mozRequestFullScreen();
+  } else if (videoContainer.webkitRequestFullscreen) {
+    videoContainer.webkitRequestFullscreen();
+  } else if (videoContainer.msRequestFullscreen) {
+    videoContainer.msRequestFullscreen();
+  }
   fullScrnBtn.innerHTML = '<i class="fas fa-compress"></i>';
   fullScrnBtn.removeEventListener("click", goFullScreen);
   fullScrnBtn.addEventListener("click", exitFullScreen);
 }
 
+/* Time 동작 */
+const formatDate = (seconds) => {
+  const secondsNumber = parseInt(seconds, 10);
+  let hours = Math.floor(secondsNumber / 3600);
+  let minutes = Math.floor((secondsNumber - hours * 3600) / 60);
+  let totalSeconds = secondsNumber - hours * 3600 - minutes * 60;
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (totalSeconds < 10) {
+    totalSeconds = `0${totalSeconds}`;
+  }
+  return `${hours}:${minutes}:${totalSeconds}`;
+};
+
+function getCurrentTime() {
+  currentTime.innerHTML = formatDate(videoPlayer.currentTime);
+  if (
+    formatDate(videoPlayer.currentTime) === formatDate(videoPlayer.duration)
+  ) {
+    playBtn.innerHTML = `<i class="fas fa-redo"></i>`;
+  }
+}
+function setTotalTime() {
+  try {
+    if (videoPlayer.duration == Infinity) {
+      totalTime.innerHTML = "";
+    } else {
+      totalTime.innerHTML = formatDate(videoPlayer.duration);
+    }
+  } catch (e) {
+    console.error(e);
+  }
+}
+
 /* init() 동작 */
 function init() {
   playBtn.addEventListener("click", handlePlayClick);
+
   volumeBtn.addEventListener("click", handleVolumeClick);
+
   fullScrnBtn.addEventListener("click", goFullScreen);
+
+  videoPlayer.addEventListener("loadeddata", setTotalTime);
+  if (videoPlayer.readyState >= 1) setTotalTime();
+  videoPlayer.addEventListener("timeupdate", getCurrentTime);
 }
 
 /* 비디오가 존재할 경우 init() 실행 */
