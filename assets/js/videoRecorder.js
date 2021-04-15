f7faff979e4efde7b2e0b3459a13d6411b46c3b6
Park-Seung-Hun <psh19930326@gmail.com>
2021-04-15 16:08:16 +0900

    #10.1 Recording Video part One

--- assets/js/videoRecorder.js
+++ assets/js/videoRecorder.js
@@ -2,25 +2,43 @@ const recorderContanier = document.getElementById("jsRecordContainer");
 const recordBtn = document.getElementById("jsRecordBtn");
 const videoPreview = document.getElementById("jsVideoPreview");
 
-/* 녹화 함수 */
-const startRecording = async () => {
+let streamObject; // stream 객체 생성
+
+/* 비디오 데이터 control */
+const handleVideoData = (event) => {
+  console.log(event);
+};
+
+/* 녹화 시작 */
+const startRecording = () => {
+  const videoRecorder = new MediaRecorder(streamObject);
+  videoRecorder.start();
+  videoRecorder.addEventListener("dataavailable", handleVideoData);
+};
+
+/* 녹화된 비디오 추출 */
+const getVideo = async () => {
   try {
     const stream = await navigator.mediaDevices.getUserMedia({
       audio: true,
       video: { width: 1280, height: 720 },
     });
     videoPreview.srcObject = stream;
-    console.log(stream);
     videoPreview.muted = true;
     videoPreview.play();
+
+    recordBtn.innerHTML = "🛑 녹화 중지";
+    streamObject = stream;
+    startRecording();
   } catch (error) {
-    recordBtn.innerHTML = "☹️ Cant record";
-    recordBtn.removeEventListener("click", startRecording);
+    recordBtn.innerHTML = "☹️ 녹화 오류";
+  } finally {
+    recordBtn.removeEventListener("click", getVideo);
   }
 };
 
 function init() {
-  recordBtn.addEventListener("click", startRecording); // 녹화 시작 버튼 클릭시 동작
+  recordBtn.addEventListener("click", getVideo); // 녹화 시작 버튼 클릭시 동작
 }
 
 if (recorderContanier) {
